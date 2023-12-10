fd415818d41f5b1223eec8e5f27ba1851269a5b8
fjus <denis_lukash@inbox.ru>
2023-12-10 14:40:12 +0300

    1 upgrade

---
+++ java/Classes/TypicalMoonCitizen.java
@@ -0,0 +1,9 @@
+package Classes;
+
+import Enums.Role;
+
+public abstract class TypicalMoonCitizen extends MoonShortie{
+    public TypicalMoonCitizen(Role role, String name) {
+        super(role, name);
+    }
+}
---
+++ java/Enums/Role.java
@@ -0,0 +1,7 @@
+package Enums;
+
+public enum Role {
+    HERO,
+    VILLAIN,
+    NEUTRAL_CHARACTER
+}
