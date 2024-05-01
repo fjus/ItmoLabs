eac061cd1796a6643ea64d24336bd02f39c71e0c
fjus <144378174+fjus@users.noreply.github.com>
2024-05-01 16:27:38 +0300

    Delete Lab6/Lab6 directory

--- Lab6/Lab6/Client/src/main/java/utils/Validatable.java
+++
@@ -1,9 +0,0 @@
-package utils;
-
-/**
- * Интерфейс Validatable. Отвечает за проверку валидности команд.
- * @author DenisLukash
- */
-public interface Validatable {
-    public boolean validate();
-}
